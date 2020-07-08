14e28a311b4497cf18825494c1d29d1d9d1b5f0d
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-07-09 02:00:54 +0800

    动画

--- myProject/App.js
+++ myProject/App.js
@@ -2,14 +2,25 @@ import React from 'react'
 import DarkLightMode from './src/DarkLightMode'
 import ProfileCard from './src/ProfileCard'
 import CitiesApp from './src/CitiesApp'
+import FocusAndScaled from './src/Animation/FocusAndScaled'
+import Loading from './src/Animation/Loading'
+import Paralle from './src/Animation/Parallel'
+import Squence from './src/Animation/Squence'
+import Stagger from './src/Animation/Stagger'
 
-console.disableYellowBox = true 
+
+console.disableYellowBox = true
 const App: () => React$Node = () => {
-  
+
   return (
     // <DarkLightMode />
     // <ProfileCard />
-      <CitiesApp />
+    // <CitiesApp />
+    // <FocusAndScaled />
+    // <Loading />
+    // <Paralle />
+    // <Squence />
+    <Stagger />
   )
 }
 
