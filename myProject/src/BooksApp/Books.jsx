00a5e5af9e317f96376d2ef0c5f60bc66462e91c
Chris Xiong <yxiong@Chriss-Mac-mini.local>
2020-07-12 23:52:09 +0800

    Add edit book

--- myProject/src/BooksApp/Books.jsx
+++ myProject/src/BooksApp/Books.jsx
@@ -8,12 +8,13 @@ import {
 } from 'react-native'
 import { connect } from 'react-redux'
 import styles from './Books.style'
-import { addBook, removeBook } from './actions'
+import { addBook, removeBook, editBook } from './actions'
 
 //初始的state
 const initialState = {
     name: '',
-    author: ''
+    author: '',
+    id: ''
 }
 
 class Books extends React.Component {
@@ -28,14 +29,24 @@ class Books extends React.Component {
     }
 
     addBook = () => {
-        //dispatchAddBook可以作为connect函数属性进行访问的函数，此函数接受整个state作为参数
-        this.props.dispatchAddBook(this.state)
+        if (!this.state.id) {
+            //dispatchAddBook可以作为connect函数属性进行访问的函数，此函数接受整个state作为参数
+            this.props.dispatchAddBook(this.state)
+        } else {
+            this.props.dispatchEditBook(this.state)
+        }
         //清空当前state的值
         this.setState(initialState)
     }
 
     removeBook = book => {
         this.props.dispatchRemoveBook(book)
+    }
+
+    selectBook = book => {
+        this.setState({
+            ...book
+        })
     }
 
     render() {
@@ -48,11 +59,13 @@ class Books extends React.Component {
                     style={styles.booksContainer}
                 >{
                         books.map((book, index) =>
-                            <View style={styles.book} key={index}>
-                                <Text style={styles.name}>{book.name}</Text>
-                                <Text style={styles.author}>{book.author}</Text>
-                                <Text onPress={() => this.removeBook(book)}>Remove</Text>
-                            </View>
+                            <TouchableOpacity onPress={() => this.selectBook(book)}>
+                                <View style={styles.book} key={index}>
+                                    <Text style={styles.name}>{book.name}</Text>
+                                    <Text style={styles.author}>{book.author}</Text>
+                                    <Text onPress={() => this.removeBook(book)}>Remove</Text>
+                                </View>
+                            </TouchableOpacity>
                         )}
                 </ScrollView>
                 <View style={styles.inputContainer}>
@@ -91,7 +104,8 @@ const mapStateToProps = state => ({ books: state.bookReducer.books })
 //mapDispatchToPropsde 的作用是将action函数直接映射到组件的属性中
 const mapDispatchToProps = {
     dispatchAddBook: book => addBook(book),
-    dispatchRemoveBook: book => removeBook(book)
+    dispatchRemoveBook: book => removeBook(book),
+    dispatchEditBook: book => editBook(book)
 }
 
 //connect第一个参数就是将redux state内容作为结果返回给当前页面的props
